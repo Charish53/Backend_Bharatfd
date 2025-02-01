38cd37555e9b230fbe1d5b96b563a76f366c410f
Charish53 <charish230@gmail.com>
2025-02-01 21:57:47 +0530

    feat: finalize API and fix issues

--- faqs/tests.py
+++ faqs/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import FAQ
 
-# Create your tests here.
+class FAQModelTest(TestCase):
+    def setUp(self):
+        self.faq = FAQ.objects.create(
+            question="What is Django?",
+            answer="Django is a Python framework."
+        )
+
+    def test_faq_creation(self):
+        self.assertEqual(self.faq.question, "What is Django?")
+        self.assertEqual(self.faq.answer, "Django is a Python framework.")
